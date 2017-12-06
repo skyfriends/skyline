08cde9d4ae0a56428f2a5543f197cf373da2e05a
Joshua Evans <skyfriends1@gmail.com>
2017-12-05 20:45:19 -0800

    fixed the link on main;

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,11 @@ import '../css/framework7-icons.css';
 import { routes } from '../routes.js';
 import { Framework7App } from 'framework7-react';
 import { Welcome } from './pages/Welcome';
+import { Company } from './pages/company-form';
+import { MainView } from './pages/main';
 
 export const App = () => (
   <Framework7App themeType="ios" routes={routes}>
-    <Welcome />
+    <MainView />
   </Framework7App>
 );
