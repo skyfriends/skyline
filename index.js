97b94cb087b4ab0ed1a15516a851799a5d7b308b
Joshua Evans <skyfriends1@gmail.com>
2017-12-06 11:52:42 -0800

    changed app to objcont

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.use(morgan('common'));
 app.use(express.static(`${__dirname}/build`));
 app.get('*', (req, res) => res.sendFile(`${__dirname}/build/index.html`));
 
-app.listen(process.env.PORT, () => {
+app.listen(3000, () => {
   console.log('__SERVER_RUNNING__', process.env.PORT);
 });
