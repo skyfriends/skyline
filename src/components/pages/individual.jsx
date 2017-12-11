147c8d9f445e9ce94779578b0ee2b0070ef28522
Dustin Byers <dbyers84@gmail.com>
2017-12-11 12:52:16 -0800

    hairlines gone, spacing between inputs set

--- src/components/pages/individual.jsx
+++ src/components/pages/individual.jsx
@@ -60,113 +60,47 @@ class Individual extends React.Component {
   }
 
   render() {
-    return (
-      <Page>
+    return <Page>
         <Navbar title="Individual" backLink="Back" sliding />
-        <List className form>
-          <ListItem className="form-list-item">
-            <FormInput
-              type="text"
-              name="name"
-              placeholder="Name"
-              onChange={this.handleChange}
-              value={this.state.name}
-            />
+        <List className="input-form-list no-hairlines-between no-hairlines">
+          <ListItem className="input-form-list-item">
+            <FormInput type="text" name="name" placeholder="Name" onChange={this.handleChange} value={this.state.name} />
           </ListItem>
 
           <ListItem className="form-list-item">
-            <FormInput
-              type="text"
-              name="email"
-              placeholder="Email"
-              cursor="none"
-              onChange={this.handleChange}
-              value={this.state.email}
-            />
+            <FormInput type="text" name="email" placeholder="Email" cursor="none" onChange={this.handleChange} value={this.state.email} />
           </ListItem>
 
           <ListItem className="form-list-item">
-            <FormInput
-              type="text"
-              name="address"
-              placeholder="Address"
-              onChange={this.handleChange}
-              value={this.state.address}
-            />
+            <FormInput type="text" name="address" placeholder="Address" onChange={this.handleChange} value={this.state.address} />
           </ListItem>
 
           <ListItem className="form-list-item">
-            <FormInput
-              type="text"
-              name="phone"
-              placeholder="Phone"
-              onChange={this.handlePhoneChange}
-              value={this.state.phone}
-            />
+            <FormInput type="text" name="phone" placeholder="Phone" onChange={this.handlePhoneChange} value={this.state.phone} />
           </ListItem>
 
           <ListItem className="form-list-item">
-            <FormInput
-              type="text"
-              name="employment"
-              placeholder="Employment"
-              onChange={this.handleChange}
-              value={this.state.employment}
-            />
+            <FormInput type="text" name="employment" placeholder="Employment" onChange={this.handleChange} value={this.state.employment} />
           </ListItem>
 
           <ListItem className="form-list-item">
-            <FormInput
-              type="text"
-              name="income"
-              placeholder="Income"
-              onChange={this.handleChange}
-              value={this.state.income}
-            />
+            <FormInput type="text" name="income" placeholder="Income" onChange={this.handleChange} value={this.state.income} />
           </ListItem>
 
           <ListItem className="form-list-item">
-            <FormInput
-              type="text"
-              name="investment"
-              placeholder="Investment"
-              onChange={this.handleChange}
-              value={this.state.investment}
-            />
+            <FormInput type="text" name="investment" placeholder="Investment" onChange={this.handleChange} value={this.state.investment} />
           </ListItem>
 
           <ListItem>
-            <FormInput
-              type="password"
-              name="password"
-              placeholder="Password"
-              onChange={this.handleChange}
-              value={this.state.password}
-              className="form-list-item"
-            />
+            <FormInput type="password" name="password" placeholder="Password" onChange={this.handleChange} value={this.state.password} className="form-list-item" />
           </ListItem>
 
-          <ListItem
-            className="form-list-item"
-            checkbox
-            name="push-notification"
-            value="1"
-            title="Enable Push Notification "
-            checked={this.state.checked}
-            onChange={this.handleChecked}
-          />
+          <ListItem className="form-list-item" checkbox name="push-notification" value="1" title="Enable Push Notification " checked={this.state.checked} onChange={this.handleChecked} />
         </List>
-        <Button
-          big
-          fill
-          href="/main/"
-          title="Company"
-          className="bottom-button"
-        >
+        <Button big fill href="/main/" title="Company" className="bottom-button">
           Continue
         </Button>
       </Page>
-    );
   }
 }
 
